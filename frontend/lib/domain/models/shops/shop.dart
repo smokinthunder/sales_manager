2acc55cc0b6786708bdbed764efdb37d7fccd11b
aadhitemp <adithyan.r@outlook.com>
2025-08-23 17:27:24 +0530

    Added url field in shop model

--- frontend/lib/domain/models/shops/shop.dart
+++ frontend/lib/domain/models/shops/shop.dart
@@ -3,10 +3,13 @@ class Shop {
   final String name;
   final String location;
   final String phoneNumber;
+  final String logoUrl;
   final int points;
   final bool needsVisiting;
   final DateTime lastVisted;
+
   Shop({
+    required this.logoUrl,
     this.needsVisiting = false,
     required this.id,
     required this.name,
