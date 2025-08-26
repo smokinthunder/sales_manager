21f24d38f7a93a2d7190b16a2c146b663fe389e4
aadhitemp <adithyan.r@outlook.com>
2025-08-26 18:23:27 +0530

    a little bit more areamanager UI'

--- frontend/lib/ui/home/home_screens/area_manager_home.dart
+++ frontend/lib/ui/home/home_screens/area_manager_home.dart
@@ -16,11 +16,6 @@ class AreaManagerHome extends ConsumerStatefulWidget {
 
 class _AreaManagerHomeState extends ConsumerState<AreaManagerHome> {
   bool _isMessageShown = false;
-  final buttonStyle = TextButton.styleFrom(
-    padding: EdgeInsets.zero,
-    minimumSize: Size(0, 0),
-    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-  );
 
   @override
   void didChangeDependencies() {
@@ -48,171 +43,285 @@ class _AreaManagerHomeState extends ConsumerState<AreaManagerHome> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Total Executive : 12", style: textTheme.headlineSmall),
-              Column(
-                spacing: 4,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextButton(
-                    style: buttonStyle,
-                    onPressed: () {
-                      context.push(RoutePaths.addShop);
-                    },
-                    child: Text(
-                      "Add new customer",
-                      style: textTheme.bodyMedium?.copyWith(
-                        color: theme.colorScheme.primary,
-                      ),
-                    ),
-                  ),
-                  TextButton(
-                    style: buttonStyle,
-                    onPressed: () {
-                      //TODO:
-                    },
-                    child: Text(
-                      "Pending Requests",
-                      style: textTheme.bodyMedium?.copyWith(
-                        color: theme.colorScheme.primary,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+          HeaderTexts(),
           const SizedBox(height: 40),
           Text("Create Route", style: Theme.of(context).textTheme.bodyLarge),
-          Card(
-            color: theme.colorScheme.onPrimary,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
+          CreateRouteCard(),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
+            child: Text(
+              "Track Executive",
+              style: Theme.of(context).textTheme.bodyLarge,
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                spacing: 16,
-                children: [
-                  Row(
-                    spacing: 10,
-                    children: [
-                      Flexible(
-                        child: CustomDropDownMenu(
-                          hintText: "Select Location",
-                          tinyTitle: true,
-                          dropdownMenuEntries: [
-                            DropdownMenuEntry(
-                              value: 'ABC Plumbing',
-                              label: 'ABC Plumbing',
-                            ),
-                            DropdownMenuEntry(
-                              value: 'XYZ Hardware',
-                              label: 'XYZ Hardware',
-                            ),
-                            DropdownMenuEntry(
-                              value: 'LMN Electricals',
-                              label: 'LMN Electricals',
-                            ),
-                          ],
-                          title: "Location",
-                        ),
-                      ),
-                      Flexible(
-                        child: CustomDropDownMenu(
-                          tinyTitle: true,
-                          hintText: "Select Area",
-                          dropdownMenuEntries: [
-                            DropdownMenuEntry(
-                              value: 'ABC Plumbing',
-                              label: 'ABC Plumbing',
-                            ),
-                            DropdownMenuEntry(
-                              value: 'XYZ Hardware',
-                              label: 'XYZ Hardware',
-                            ),
-                            DropdownMenuEntry(
-                              value: 'LMN Electricals',
-                              label: 'LMN Electricals',
-                            ),
-                          ],
-                          title: "Area",
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    spacing: 10,
-                    children: [
-                      Flexible(
-                        child: CustomDropDownMenu(
-                          tinyTitle: true,
-                          hintText: "Select Executive",
-                          dropdownMenuEntries: [],
-                          title: "Location",
-                        ),
-                      ),
-                      Flexible(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 4.0,
-                              ),
-                              child: Text("Date", style: textTheme.bodySmall),
-                            ),
-                            TextField(
-                              decoration: InputDecoration(
-                                contentPadding: EdgeInsets.symmetric(
-                                  horizontal: 8,
-                                  vertical: 12,
-                                ),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                  borderSide: BorderSide(
-                                    color: colorScheme.tertiary,
-                                  ),
-                                ),
-                                hint: Text(
-                                  'mm/dd/yyyy',
-                                  style: textTheme.labelLarge,
-                                ),
-                                suffixIcon: Icon(
-                                  Icons.calendar_month,
-                                  color: colorScheme.tertiary,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          //TODO:
-                        },
-                        child: Text("Submit"),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          //TODO
-                        },
-                        child: Text('Add special route'),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+          ),
+          TrackExecutiveCard(),
+          TextButton(
+            onPressed: () {
+              //TODO
+            },
+            child: Row(
+              spacing: 10,
+              children: [
+                Text("Create Target"),
+                Icon(Icons.arrow_forward_ios_outlined),
+              ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class HeaderTexts extends StatelessWidget {
+  const HeaderTexts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final buttonStyle = TextButton.styleFrom(
+      padding: EdgeInsets.zero,
+      minimumSize: Size(0, 0),
+      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    );
+    final ThemeData theme = Theme.of(context);
+    final TextTheme textTheme = theme.textTheme;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text("Total Executive : 12", style: textTheme.headlineSmall),
+        Column(
+          spacing: 4,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextButton(
+              style: buttonStyle,
+              onPressed: () {
+                context.push(RoutePaths.addShop);
+              },
+              child: Text(
+                "Add new customer",
+                style: textTheme.bodyMedium?.copyWith(
+                  color: theme.colorScheme.primary,
+                ),
+              ),
+            ),
+            TextButton(
+              style: buttonStyle,
+              onPressed: () {
+                //TODO:
+              },
+              child: Text(
+                "Pending Requests",
+                style: textTheme.bodyMedium?.copyWith(
+                  color: theme.colorScheme.primary,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class CreateRouteCard extends StatelessWidget {
+  const CreateRouteCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final TextTheme textTheme = theme.textTheme;
+    final ColorScheme colorScheme = theme.colorScheme;
+    return Card(
+      color: theme.colorScheme.onPrimary,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          spacing: 16,
+          children: [
+            Row(
+              spacing: 10,
+              children: [
+                Flexible(
+                  child: CustomDropDownMenu(
+                    hintText: "Select Location",
+                    tinyTitle: true,
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry(
+                        value: 'ABC Plumbing',
+                        label: 'ABC Plumbing',
+                      ),
+                      DropdownMenuEntry(
+                        value: 'XYZ Hardware',
+                        label: 'XYZ Hardware',
+                      ),
+                      DropdownMenuEntry(
+                        value: 'LMN Electricals',
+                        label: 'LMN Electricals',
+                      ),
+                    ],
+                    title: "Location",
+                  ),
+                ),
+                Flexible(
+                  child: CustomDropDownMenu(
+                    tinyTitle: true,
+                    hintText: "Select Area",
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry(
+                        value: 'ABC Plumbing',
+                        label: 'ABC Plumbing',
+                      ),
+                      DropdownMenuEntry(
+                        value: 'XYZ Hardware',
+                        label: 'XYZ Hardware',
+                      ),
+                      DropdownMenuEntry(
+                        value: 'LMN Electricals',
+                        label: 'LMN Electricals',
+                      ),
+                    ],
+                    title: "Area",
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              spacing: 10,
+              children: [
+                Flexible(
+                  child: CustomDropDownMenu(
+                    tinyTitle: true,
+                    hintText: "Select Executive",
+                    dropdownMenuEntries: [],
+                    title: "Location",
+                  ),
+                ),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4.0),
+                        child: Text("Date", style: textTheme.bodySmall),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 12,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: colorScheme.tertiary),
+                          ),
+                          hint: Text('mm/dd/yyyy', style: textTheme.labelLarge),
+                          suffixIcon: Icon(
+                            Icons.calendar_month,
+                            color: colorScheme.tertiary,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    //TODO:
+                  },
+                  child: Text("Submit"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    //TODO
+                  },
+                  child: Text('Add special route'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TrackExecutiveCard extends StatelessWidget {
+  const TrackExecutiveCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Card(
+      color: theme.colorScheme.onPrimary,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          spacing: 16,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              spacing: 10,
+              children: [
+                Flexible(
+                  child: CustomDropDownMenu(
+                    hintText: "Executive list",
+                    tinyTitle: true,
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry(
+                        value: 'ABC Plumbing',
+                        label: 'ABC Plumbing',
+                      ),
+                      DropdownMenuEntry(
+                        value: 'XYZ Hardware',
+                        label: 'XYZ Hardware',
+                      ),
+                      DropdownMenuEntry(
+                        value: 'LMN Electricals',
+                        label: 'LMN Electricals',
+                      ),
+                    ],
+                    title: "Select executive to track",
+                  ),
+                ),
+                Flexible(
+                  child: CustomDropDownMenu(
+                    tinyTitle: true,
+                    hintText: "Shop name",
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry(
+                        value: 'ABC Plumbing',
+                        label: 'ABC Plumbing',
+                      ),
+                      DropdownMenuEntry(
+                        value: 'XYZ Hardware',
+                        label: 'XYZ Hardware',
+                      ),
+                      DropdownMenuEntry(
+                        value: 'LMN Electricals',
+                        label: 'LMN Electricals',
+                      ),
+                    ],
+                    title: "Select a shop",
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                //TODO:
+              },
+              child: Text("Submit"),
+            ),
+          ],
+        ),
       ),
     );
   }
