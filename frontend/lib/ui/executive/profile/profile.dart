e84ab01896cf96d6e131087bc5173097ceb0620b
Antesh <115895085+smokinthunder@users.noreply.github.com>
2025-08-29 14:26:55 +0530

    Resolved the merge conflict in the file.

--- frontend/lib/ui/executive/profile/profile.dart
+++ frontend/lib/ui/executive/profile/profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sales_manager/config/providers/current_user_notifier.dart';
 import 'package:sales_manager/domain/models/user/user.dart';
+import 'package:sales_manager/ui/core/colors.dart';
 import 'package:sales_manager/ui/profile/profile_view_model.dart';
 
 class ExecutiveProfile extends ConsumerStatefulWidget {
@@ -45,7 +46,21 @@ class _ExecutiveProfileState extends ConsumerState<ExecutiveProfile> {
     final theme = Theme.of(context);
     return Stack(
       children: [
-        Container(height: 160, color: theme.colorScheme.primary),
+        Container(
+          height: 160,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                AppColors.blue,
+                AppColors.green,
+                AppColors.white,
+              ], //TODO: fix/make consistent with design
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.0, 0.7, 1.0],
+            ),
+          ),
+        ),
         SingleChildScrollView(
           child: Column(
             children: [
@@ -130,54 +145,6 @@ class _ExecutiveProfileState extends ConsumerState<ExecutiveProfile> {
                 ),
               ),
             ],
-          ),
-        ),
-        Positioned(
-          right: -60,
-          top: 0,
-          child: Container(
-            width: 120,
-            height: 120,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Color(0xff1479ba),
-            ),
-          ),
-        ),
-        Positioned(
-          left: -15,
-          top: 100,
-          child: Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Color(0xff1479ba),
-            ),
-          ),
-        ),
-        Positioned(
-          right: 130,
-          top: 5,
-          child: Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Color(0xff1479ba),
-            ),
-          ),
-        ),
-        Positioned(
-          left: 40,
-          top: 5,
-          child: Container(
-            width: 70,
-            height: 70,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Color(0xff1479ba),
-            ),
           ),
         ),
       ],
